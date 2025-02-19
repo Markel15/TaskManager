acf41e71620b7f0d551dd4ff011546ebd5cba17f
Markel15 <enemar.h@gmail.com>
2025-02-19 18:56:09 +0100

    Botones de toolbar

--- app/src/main/java/com/example/proyecto/MainActivity.java
+++ app/src/main/java/com/example/proyecto/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.proyecto;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +17,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        setSupportActionBar(findViewById(R.id.labarra));
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.definicion_menu,menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.opcion1) {
+            // Acción para "Favorito"
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
