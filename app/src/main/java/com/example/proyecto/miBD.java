3dbcfa34a4ae1cb7102060fbe94c5372d155b672
Markel15 <enemar.h@gmail.com>
2025-02-22 03:25:21 +0100

    Crear clase de base de datos (singleton)

---
+++ app/src/main/java/com/example/proyecto/miBD.java
@@ -0,0 +1,46 @@
+package com.example.proyecto;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class miBD extends SQLiteOpenHelper {
+
+    private static final String NOMBRE = "tareas.db";
+    private static final int VERSION = 1;
+
+    private static miBD miBD = null;
+
+    private miBD(@Nullable Context context) {
+        super(context, NOMBRE, null, VERSION);
+    }
+
+    public static miBD getMiBD(Context context) {
+        if (miBD == null) {
+            miBD = new miBD(context.getApplicationContext());
+        }
+        return miBD;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String comando = "CREATE TABLE tareas (" +
+                "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "titulo TEXT NOT NULL, " +
+                "descripcion TEXT, " +
+                "fechaCreacion INTEGER, " +
+                "FechaFinalizacion INTEGER, " +
+                "completeado INTEGER DEFAULT 0, " +
+                "prioridad INTEGER DEFAULT 0" +
+                ");";
+        db.execSQL(comando);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS tareas");
+        onCreate(db);
+    }
+}
