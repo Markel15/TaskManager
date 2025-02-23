d476c5294f28eb02b84defd0db7f537fe6623a0d
Markel15 <enemar.h@gmail.com>
2025-02-23 03:12:00 +0100

    Añadir botón inicio de sesión

--- app/src/main/java/com/example/proyecto/LoginActivity.java
+++ app/src/main/java/com/example/proyecto/LoginActivity.java
@@ -9,13 +9,13 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
     private EditText etUsername, etPassword;  // editTexts
     private Button btnLogin, btnRegister;
+
+    miBD miDb = miBD.getMiBD(this);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +26,28 @@ public class LoginActivity extends AppCompatActivity {
         etPassword = findViewById(R.id.etPassword);
         btnLogin   = findViewById(R.id.btnLogin);
         btnRegister = findViewById(R.id.btnRegister);
+
+        btnLogin.setOnClickListener(v -> {
+            String username = etUsername.getText().toString().trim();
+            String password = etPassword.getText().toString().trim();
+            if (loginUser(username, password)) {
+                // Usuario autenticado: inicia la MainActivity
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            } else {
+                Toast.makeText(LoginActivity.this, "Credenciales incorrectas", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private boolean loginUser(String username, String password) {
+        SQLiteDatabase db = miDb.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM usuarios WHERE username = ? AND password = ?",
+                new String[]{username, password});
+        boolean resultado = cursor.moveToFirst();
+        cursor.close();
+        return resultado;
     }
 
 }
